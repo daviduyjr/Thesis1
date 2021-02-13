7119d585818052aaa0a9a24b035e35eb9afe9606
David <daviduy2nd@gmail.com>
2021-02-13 12:25:04 +0800

    wala lang

--- server/models/inventory/counter.js
+++ server/models/inventory/counter.js
@@ -1,28 +1,8 @@
 var mongoose = require("mongoose");
 
-// define the schema for our user model
 var counterSchema = mongoose.Schema({
   id: { type: String, required: true },
   seq: { type: Number, default: 0 },
 });
 
-// counterSchema.index({ _id: 1, seq: 1 }, { unique: true });
-
-// const counterModel = mongoose.model("counter", counterSchema);
-
-// const autoIncrementModelID = function (modelName, doc, next) {
-//   counterModel.findByIdAndUpdate(
-//     modelName,
-//     { $inc: { seq: 1 } },
-//     { new: true, upsert: true },
-//     function (error, counter) {
-//       if (error) return next(error);
-
-//       doc.id = counter.seq;
-//       next();
-//     }
-//   );
-// };
-
-// module.exports = autoIncrementModelID;
 module.exports = mongoose.model("counter", counterSchema);
